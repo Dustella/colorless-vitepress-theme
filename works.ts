ece16d988cac2367c12d01b29a0acc4f7d57894f
Dustella <fdnoaivj@outlook.com>
2023-03-27 23:55:30 +0800

    ✨ add more details

--- works.ts
+++ works.ts
@@ -6,22 +6,43 @@ interface MyWorks {
 }
 
 const works: MyWorks = {
-  运营的服务: [
+  第一个项目: [
     {
-      title: '特菈的杂货铺',
-      details: '就是一个杂货铺',
+      title: '一些玩具',
+      details: '一个个人作品',
       link: 'https://index.dustella.net',
       icon: { src: 'https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg', height: '15px' },
     },
     {
 
-      title: 'NUISTCraft',
-      details: '一个NUIST的Minecraft服务器',
+      title: 'lalala',
+      details: '很新的东西',
       link: 'https://nuistcraft.com',
       icon: { src: 'https://nuistcraft.com/img/undraw_docusaurus_mountain.svg', height: '15px' },
     },
   ],
-  个人项目: [],
+  个人项目: [
+    {
+      title: '一些玩具',
+      details: '一个个人作品',
+      link: 'https://index.dustella.net',
+      icon: { src: 'https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg', height: '15px' },
+    },
+    {
+
+      title: 'lalala',
+      details: '很新的东西',
+      link: 'https://nuistcraft.com',
+      icon: { src: 'https://nuistcraft.com/img/undraw_docusaurus_mountain.svg', height: '15px' },
+    },
+    {
+      title: '一些玩具',
+      details: '一个个人作品',
+      link: 'https://index.dustella.net',
+      icon: { src: 'https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg', height: '15px' },
+    },
+
+  ],
 }
 
 export default works
